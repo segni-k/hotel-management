dedb0ceb7e77bf3ef375d979682e64b81fe82e9e
Segni Kasahun <segnikasahun969@gmail.com>
2025-07-18 22:26:52 +0200

    removing installed @next/font to use built in next js font

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
